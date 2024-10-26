2122a2c868b1a7a10e51f20f7dc7a644589662e4
andreeezinho <andreeesapucaia@gmail.com>
2024-10-26 17:22:47 -0300

    feat: Adiciona migrations para construção do database

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//importando model Tarefa
+use App\Models\Tarefa;
+
+class TarefaController extends Controller
+{
+    //
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('titulo', 'Home')
+
+@section('content')
+    <h1>HOME</h1>
+@endsection
